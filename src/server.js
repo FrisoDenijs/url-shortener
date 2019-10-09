f0ed2c9e4865a847dcd1dce777da58fa0780d42d
Friso Dev <frisodev@Frisos-MacBook-Pro.local>
2019-10-09 09:16:05 +0200

    added linting

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const port = process.env.port || 3000;
 
+// eslint-disable-next-line no-unused-vars
 const server = app.listen(port, () => {
-    console.log('Server is up and running on port ' + port);
-})
+  console.log('Server is up and running on port ' + port);
+});
