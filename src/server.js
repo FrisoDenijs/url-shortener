a74980aab373a72e8558e140f330e461094e5b41
Friso Denijs <frisodenijs@gmail.com>
2019-10-09 15:26:11 +0200

    cleanup server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
-//import express from 'express';
-
-
-
 const mongooseSetup = require('./setups/mongoose.setup');
 const expressSetup = require('./setups/express.setup');
 
 mongooseSetup.setup();
 const app = expressSetup.setup();
-
-
 
 // eslint-disable-next-line no-undef
 const port = process.env.port || 3000;
