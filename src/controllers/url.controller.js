db86e5f415743db4541bad8e659f79b794f15256
Friso Denijs <frisodenijs@gmail.com>
2019-10-10 10:42:03 +0200

    seperated redirect route from url route

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -23,12 +23,3 @@ exports.create = (req, res) => {
 		res.status(200).send(url);
 	});
 }
-
-exports.redirect = (req, res) => {
-	Url.findOne({ shortUrl: req.params.url}, function (err, url) {
-		if (err) {
-			res.status(400).send(err);
-		}
-		url && url.fullUrl ? res.redirect(url.fullUrl) : res.status(404).send();
-    })
-}
--- src/setups/express/routes.setup.js
+++ src/setups/express/routes.setup.js
@@ -1,7 +1,9 @@
 const urlRoute = require('../../routes/url.route');
 const viewRoute = require ('../../routes/views.route');
+const redirectRoute = require ('../../routes/redirect.route');
 
 exports.setup = (app) => {
     app.use('/', viewRoute);
+    app.use('/', redirectRoute);
     app.use('/api/url', urlRoute);
 }
